b2ac2ad47ec6f3e06da27bcb078de6f341f9f824
SK ISMILE <101392872+skismile@users.noreply.github.com>
2022-12-13 19:08:38 +0530

    add

--- Router/User.Router.js
+++ Router/User.Router.js
@@ -3,60 +3,77 @@ const app = express.Router();
 const UserModel = require("../Model/User.Model");
 
 app.get("/", async (req, res) => {
-  console.log("user")
+  console.log("user");
   res.send("USER");
 });
 
 // Signup route
 
-
-
 app.post("/signup", async (req, res) => {
-  console.log("signup")
+  console.log("signup");
   const { name, email, password } = req.body;
 
   try {
-    const checkEmail = await UserModel.findOne({ email:email });
-    if (checkEmail) {
-      return res.status(401).send("User email already exist");
-    } else {
-      try {
-        const user = new UserModel({
-          name,
-          email,
-          password,
-        });
-        await user.save();
-        console.log(user);
-        return res.send("user created successfully");
-      } catch (err) {
-        console.log(err);
-        return res.status(401).send("invalid cred");
-      }
-    }
-  } catch (e) {
-    console.log(e);
-    res.status(401).send(e.message);
+    const user = new UserModel({
+      name:name,
+      email:email,
+      password:password,
+    });
+    await user.save();
+    console.log(user);
+    return res.send("user created successfully");
+  } catch (err) {
+    console.log(err);
+    return res.status(401).send("invalid cred");
   }
 });
+
+// app.post("/signup", async (req, res) => {
+//   console.log("signup")
+//   const { name, email, password } = req.body;
+
+//   try {
+//     const checkEmail = await UserModel.findOne({ email:email });
+//     if (checkEmail) {
+//       return res.status(401).send("User email already exist");
+//     } else {
+//       try {
+//         const user = new UserModel({
+//           name,
+//           email,
+//           password,
+//         });
+//         await user.save();
+//         console.log(user);
+//         return res.send("user created successfully");
+//       } catch (err) {
+//         console.log(err);
+//         return res.status(401).send("invalid cred");
+//       }
+//     }
+//   } catch (e) {
+//     console.log(e);
+//     res.status(401).send(e.message);
+//   }
+// });
 
 // //Signin Route
-app.post("/signin", async (req, res) => {
-  const { email, password } = req.body;
+// app.post("/signin", async (req, res) => {
+//   const { email, password } = req.body;
 
-  try {
-    const user = await UserModel.findOne({ email, password });
+//   try {
+//     const user = await UserModel.findOne({ email, password });
 
-    return res.send({
-      message: "login Successfully",
-      name: user.name,
-      email: user.email,
-      id: user._id,
-      date: user.date,
-    });
-  } catch (e) {
-    return res.status(401).send("invalid credential");
-  }
-});
+//     return res.send({
+//       message: "login Successfully",
+//       name: user.name,
+//       email: user.email,
+//       id: user._id,
+//       date: user.date,
+//     });
+//   } catch (e) {
+//     return res.status(401).send("invalid credential");
+//   }
+// });
 
 module.exports = app;
