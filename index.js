c1e698bd5dad3ee4dfeacb1da2de485d8c0ce3ba
SK ISMILE <101392872+skismile@users.noreply.github.com>
2022-12-13 19:19:35 +0530

    add

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const EmiRouter=require("./Router/Emi.Router")
 const cors=require("cors")
 const app=express()
 const PORT=process.env.PORT||8080
+const MONGO_URL=process.env.MONGO_URL
 app.use(express.urlencoded({extended : true}))
 app.use(cors());
 app.use(express.json())
@@ -15,7 +16,7 @@ app.get('/',(req,res)=>{res.send('hello')})
 mongoose.set("strictQuery", false);
  
 
-mongoose.connect("mongodb+srv://skismile:7867@cluster0.qqyyxrv.mongodb.net/?retryWrites=true&w=majority",()=>{
+mongoose.connect(MONGO_URL,()=>{
 
     
     app.listen(PORT,()=>{console.log('server is runing on port 8080')})
